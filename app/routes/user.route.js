065a08e0a29fff513264e05b89e0b5dfc1eaf6ea
= <=>
2016-01-22 15:22:41 +0100

    add orutes for logout and login

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -7,6 +7,12 @@ function userRoutes (router) {
 	router.route('/users/')
 		.post(userCtrl.register);
 
+  router.route('/users/login')
+    .post(auth.login)
+
+  router.route('/users/logout')
+    .post(auth.authenticate, auth.logout)  
+  
   router.route('/users/:id')
     .get(auth.authenticate, userCtrl.getUser)
     .put(auth.authenticate, userCtrl.updateProfile)
